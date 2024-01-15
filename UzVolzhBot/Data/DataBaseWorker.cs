950282485977b85b008b1305c25721d2e610d4e2
Vorobiev Valentine <q80665646662@gmail.com>
2024-01-15 15:50:54 +0400

    Фиксация

--- UzVolzhBot/Data/DataBaseWorker.cs
+++ UzVolzhBot/Data/DataBaseWorker.cs
@@ -385,7 +385,7 @@ namespace TelegramBotClean.Data
             }
             return false;
         }
-
+        //
         //Commands
         public long GetIdCommandByName(Command c)
         {
